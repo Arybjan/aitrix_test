4b442d2ba591942d12934e57eba44e344280db73
Arybjan <arybjen92@gmail.com>
2022-04-16 17:45:06 +0600

    create models for api

--- task/admin.py
+++ task/admin.py
@@ -1,3 +1,20 @@
 from django.contrib import admin
+from task.models import Category, Subcategory, Advertisement
+
 
 # Register your models here.
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ["id", "title"]
+
+
+class SubcategoryAdmin(admin.ModelAdmin):
+    list_display = ["category", "title"]
+
+
+class AdvertisementAdmin(admin.ModelAdmin):
+    list_display = ["id", "title", "description", "category"]
+
+
+admin.site.register(Category, CategoryAdmin)
+admin.site.register(Subcategory, SubcategoryAdmin)
+admin.site.register(Advertisement, AdvertisementAdmin)
--- task/models.py
+++ task/models.py
@@ -1,3 +1,17 @@
 from django.db import models
 
+
 # Create your models here.
+class Category(models.Model):
+    title = models.CharField(max_length=200)
+
+
+class Subcategory(models.Model):
+    category = models.CharField(primary_key="task.Category", max_length=140)
+    title = models.CharField(max_length=200)
+
+
+class Advertisement(models.Model):
+    title = models.CharField(max_length=240)
+    description = models.TextField()
+    category = models.ForeignKey("task.Subcategory", on_delete=models.CASCADE)
