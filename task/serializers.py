3031722849f0812e861a442798ca9701bea53c05
Arybjan <arybjen92@gmail.com>
2022-04-16 17:56:56 +0600

    add serializers and views

--- task/serializers.py
+++ task/serializers.py
@@ -1,1 +1,20 @@
+from rest_framework import serializers
+from task import models
 
+
+class CategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Category
+        fields = "__all__"
+
+
+class SubcategorySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Subcategory
+        fields = "__all__"
+
+
+class AdvertisementSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = models.Advertisement
+        fields = "__all__"
--- task/views.py
+++ task/views.py
@@ -1,3 +1,19 @@
-from django.shortcuts import render
+from rest_framework import viewsets
 
-# Create your views here.
+from task.models import Category, Subcategory, Advertisement
+from task.serializers import CategorySerializer, SubcategorySerializer, AdvertisementSerializer
+
+
+class CategoryViewSet(viewsets.ModelViewSet):
+    queryset = Category.objects.all()
+    serializer_class = CategorySerializer
+
+
+class SubcategoryViewSet(viewsets.ModelViewSet):
+    queryset = Subcategory.oblects.all()
+    serializer_class = SubcategorySerializer
+
+
+class AdvertisementViewSet(viewsets.ModelViewSet):
+    queryset = Advertisement.objects.all()
+    serializer_class = AdvertisementSerializer
